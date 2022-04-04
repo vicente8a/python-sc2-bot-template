ce9763b7d61a4eddd13162f05429080f0d7bcd0d
vicente8a <vicente8alorenzo@gmail.com>
2022-04-03 20:09:40 -0700

    added some includes

--- bot/bot.py
+++ bot/bot.py
@@ -1,5 +1,12 @@
+import sc2
 from sc2 import BotAI, Race
-
+from sc2.ids.unit_typeid import UnitTypeId
+from sc2.ids.ability_id import AbilityId
+from sc2.ids.upgrade_id import UpgradeId
+from sc2.unit import Unit
+from sc2.units import Units
+from sc2.position import Point2
+from sc2.player import Bot, Computer
 
 class CompetitiveBot(BotAI):
     NAME: str = "CompetitiveBot"
@@ -12,6 +19,9 @@ class CompetitiveBot(BotAI):
         Race.Protoss
         Race.Random
     """
+    def __init__(self):
+        # Initialize inherited class
+        sc2.BotAI.__init__(self)
 
     async def on_start(self):
         print("Game started")
